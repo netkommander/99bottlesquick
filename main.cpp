294d2cc50f462695ca8fddb776fcc4ca40a9f7ff
Ular Nebulas <64513006+netkommander@users.noreply.github.com>
2020-05-14 15:48:56 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/* Better Version 5.14.20 */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Write a for loop here:
+  int i=100;
+  for(i;i>0;)
+  {
+    std::cout << i << " bottles.\n";
+    std::cout << "Take one pass it.\n";
+    i--;
+
+    if(i == 0)
+    {
+        std::cout << "No more bottles of pop on the wall.\n";
+  std::cout << "No more bottles of pop.\n";
+  std::cout << "Go to the store and buy some more,\n";
+  std::cout << "99 bottles of pop on the wall.\n";
+    }
+  }
+}
+
+
+
